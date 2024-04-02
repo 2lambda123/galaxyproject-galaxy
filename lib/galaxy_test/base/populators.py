mvdbeek/galaxy

Skip staging inputs for outputs

lib/galaxy_test/base/populators.py
@@ -409,7 +409,8 @@ class CwlPopulator:
         job: Optional[Dict] = None,
         test_data_directory: Optional[str] = None,
         history_id: Optional[str] = None,
-        assert_ok=True,
+        skip_input_staging: bool = False,
+        assert_ok: bool = True,
     ):
         """
         :param artifact: CWL tool id, or (absolute or relative) path to a CWL
@@ -436,16 +437,17 @@ class CwlPopulator:
                     job = json.load(f)
         elif job is None:
             job = {}
-        _, datasets_uploaded = stage_inputs(
-            self.dataset_populator.galaxy_interactor,
-            history_id,
-            job,
-            use_fetch_api=False,
-            tool_or_workflow=tool_or_workflow,
-            job_dir=test_data_directory,
-        )
-        if datasets_uploaded:
-            self.dataset_populator.wait_for_history(history_id=history_id, assert_ok=True)
+        if not skip_input_staging:
+            _, datasets_uploaded = stage_inputs(
+                self.dataset_populator.galaxy_interactor,
+                history_id,
+                job,
+                use_fetch_api=False,
+                tool_or_workflow=tool_or_workflow,
+                job_dir=test_data_directory,
+            )
+            if datasets_uploaded:
+                self.dataset_populator.wait_for_history(history_id=history_id, assert_ok=True)
         if tool_or_workflow == "tool":
             run_object = self._run_cwl_tool_job(
                 artifact,

2lambda123/galaxyproject-galaxy

lib/galaxy_test/base/populators.py
@@ -2392,6 +2392,7 @@ class CwlPopulator:
         job: Optional[Dict] = None,
         test_data_directory: Optional[str] = None,
         history_id: Optional[str] = None,
+        skip_input_staging: bool = False,
         assert_ok: bool = True,
     ) -> CwlRun:
         """
@@ -2416,16 +2417,17 @@ class CwlPopulator:
                 job = yaml.safe_load(f)
         elif job is None:
             job = {}
-        _, datasets = stage_inputs(
-            self.dataset_populator.galaxy_interactor,
-            history_id,
-            job,
-            use_fetch_api=False,
-            tool_or_workflow=tool_or_workflow,
-            job_dir=test_data_directory,
-        )
-        if datasets:
-            self.dataset_populator.wait_for_history(history_id=history_id, assert_ok=True)
+        if not skip_input_staging:
+            _, datasets = stage_inputs(
+                self.dataset_populator.galaxy_interactor,
+                history_id,
+                job,
+                use_fetch_api=False,
+                tool_or_workflow=tool_or_workflow,
+                job_dir=test_data_directory,
+            )
+            if datasets:
+                self.dataset_populator.wait_for_history(history_id=history_id, assert_ok=True)
         if tool_or_workflow == "tool":
             run_object = self._run_cwl_tool_job(
                 artifact,