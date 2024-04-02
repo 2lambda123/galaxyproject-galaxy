common-workflow-lab/galaxy

Fix directory location tests

lib/galaxy/tool_util/cwl/util.py
@@ -54,6 +54,7 @@ def output_properties(path=None, content=None, basename=None, pseduo_location=Fa
 
 def _handle_pseudo_location(properties, pseduo_location):
     if pseduo_location:
+        # TODO: should be a URI to the dataset on the server
         properties["location"] = properties["basename"]
 
 
@@ -528,6 +529,7 @@ def output_to_cwl_json(
                     "basename": basename,
                     "listing": listing,
                 }
+                _handle_pseudo_location(properties, pseduo_location=pseduo_location)
 
                 extra_files = get_extra_files(output_metadata)
                 for extra_file in extra_files:

2lambda123/galaxyproject-galaxy

lib/galaxy/tool_util/cwl/util.py
@@ -97,6 +97,7 @@ def output_properties(
 
 def _handle_pseudo_location(properties, pseudo_location):
     if pseudo_location:
+        # TODO: should be a URI to the dataset on the server
         properties["location"] = properties["basename"]
 
 
@@ -590,6 +591,7 @@ def output_to_cwl_json(
                     "basename": basename,
                     "listing": listing,
                 }
+                _handle_pseudo_location(properties, pseudo_location=pseudo_location)
 
                 extra_files = get_extra_files(output_metadata)
                 for extra_file in extra_files: